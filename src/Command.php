a39567426e5199490647810cbf2598ccc1214120
SebSept <sebastienmonterisi@yahoo.fr>
2021-04-11 21:53:43 +0200

    c'est un PR.
    Signed-off-by: SebSept <sebastienmonterisi@yahoo.fr>

--- src/Command.php
+++ src/Command.php
@@ -24,6 +24,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+// feature on va dire...
 /**
  * Symfony Command class for PrestaShop allowed to rely on legacy classes
  */
